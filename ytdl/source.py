f5f9a0849bbae06e142f8860a46bcd5727f11032
bunga <bungamungil@outlook.com>
2021-10-29 19:34:54 +0700

    Separate function to extract info from url, useful for upcoming playlist feature

--- ytdl/source.py
+++ ytdl/source.py
@@ -13,9 +13,15 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=False):
+        data = (await YTDLSource.extract_info(url, loop=loop, stream=stream))[0]
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+    @classmethod
+    async def extract_info(cls, url, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
         if 'entries' in data:
-            data = data['entries'][0]
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+            return data['entries']
+        else:
+            return [data]
